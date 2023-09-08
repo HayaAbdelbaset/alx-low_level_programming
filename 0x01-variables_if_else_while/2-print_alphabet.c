0c5c2665d74d300b510aea7c8b05bf9cb2602d1d
HayaAbdelbaset <haia_elbadwy@yahoo.com>
2023-09-08 16:51:44 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - This is the task
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char C;
+
+	C = 'a';
+	while
+		(C <= 'z') {
+			putchar(C);
+			C++;
+		}
+	putchar('\n');
+	return (0);
+}
