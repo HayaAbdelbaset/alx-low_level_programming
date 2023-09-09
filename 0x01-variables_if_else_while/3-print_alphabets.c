d6017dbe80ce5bf78545e38f79ffc107b039ada0
HayaAbdelbaset <haia_elbadwy@yahoo.com>
2023-09-08 17:12:34 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - The third task
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char C;
+
+	char d;
+
+	C = 'a';
+	d = 'A';
+	while
+		(C <= 'z') {
+			putchar(C);
+			C++;
+		}
+	while
+		(d <= 'z') {
+			putchar (d);
+			d++;
+		}
+	putchar('\n');
+	return (0);
+}
